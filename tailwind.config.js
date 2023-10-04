4e6819a32ba0e890dc0671a92c724ad1892faaaa
Tamas Schneider <snaaajder@gmail.com>
2023-10-04 17:38:37 +0100

    Add colors to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#131314',
+        text: '#EBE9FC',
+        primary: '#2B1B31',
+        secondary: '#151338',
+        outline: '#41294B',
+        accent: '#4455DC',
+      }
+    }
   },
   plugins: []
 }
