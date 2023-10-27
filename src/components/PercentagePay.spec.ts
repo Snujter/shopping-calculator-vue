dcc6f7ac9d816d5a38ee672695a8d3002a2bf959
Tamas Schneider <schneider.tamas1995@gmail.com>
2023-10-27 02:10:01 +0100

    Add PercentagePay component

---
+++ src/components/PercentagePay.spec.ts
@@ -0,0 +1,26 @@
+import { describe, it, expect, assert } from 'vitest'
+
+import { shallowMount } from '@vue/test-utils'
+import PercentagePay from '@/components/PercentagePay.vue'
+
+describe('PercentagePay.vue', () => {
+  it('has default prop values', () => {
+    const wrapper = shallowMount(PercentagePay)
+
+    assert.equal(wrapper.props().price, 0, 'Default price is not 0')
+    assert.equal(wrapper.props().percentage, 0, 'Default percentage is not 0')
+  })
+
+  it('emits events', async () => {
+    const wrapper = shallowMount(PercentagePay)
+
+    // events to emit
+    wrapper.vm.$emit('update:percentage', 3)
+
+    // wait until emitted
+    await wrapper.vm.$nextTick()
+
+    // check if emitted values are correct
+    expect(wrapper.emitted('update:percentage')?.[0]).toStrictEqual([3])
+  })
+})
