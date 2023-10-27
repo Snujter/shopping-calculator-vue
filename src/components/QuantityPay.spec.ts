076f9c3dc12e2e7586f7e64801cb610e520b7012
Snajder <snaaajder@gmail.com>
2023-10-27 02:01:12 +0100

    Add QuantityPay component

---
+++ src/components/QuantityPay.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect, assert } from 'vitest'
+
+import { shallowMount } from '@vue/test-utils'
+import QuantityPay from '@/components/QuantityPay.vue'
+
+describe('QuantityPay.vue', () => {
+  it('has default prop values', () => {
+    const wrapper = shallowMount(QuantityPay)
+
+    assert.equal(wrapper.props().price, 0, 'Default price is not 0')
+    assert.equal(wrapper.props().quantity, 0, 'Default quantity is not 0')
+    assert.equal(wrapper.props().maxQuantity, undefined, 'Default maxQuantity is not undefined')
+  })
+
+  it('emits events', async () => {
+    const wrapper = shallowMount(QuantityPay)
+
+    // events to emit
+    wrapper.vm.$emit('update:quantity', 3)
+
+    // wait until emitted
+    await wrapper.vm.$nextTick()
+
+    // check if emitted values are correct
+    expect(wrapper.emitted('update:quantity')?.[0]).toStrictEqual([3])
+  })
+})
---
+++ src/components/QuantityPay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class='flex flex-col p-8'
+    :class='[price > 0 ? "bg-opacity-30 bg-primary" : ""]'
+  >
+    <NumberInput class='mb-2' :value='quantity' @input='handleQuantityChange' :max='maxQuantity' />
+    <ItemPrice :price='price' :class='{"text-gray-600": price == 0}' />
+  </div>
+</template>
+<script setup lang='ts'>
+import ItemPrice from '@/components/ItemPrice.vue'
+import NumberInput from '@/components/inputs/NumberInput.vue'
+
+defineProps({
+  price: { type: Number, default: 0 },
+  quantity: { type: Number, default: 0 },
+  maxQuantity: { type: Number, default: undefined },
+})
+
+const emit = defineEmits(['update:quantity'])
+
+const handleQuantityChange = (newQuantity: number) => {
+  emit('update:quantity', newQuantity)
+}
+</script>
