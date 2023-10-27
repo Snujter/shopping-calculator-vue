fc016a3500d7a1356efec5a33842e7621fe6462b
Snajder <snaaajder@gmail.com>
2023-10-27 02:10:01 +0100

    Add PercentagePay component

---
+++ src/components/PercentagePay.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class='flex flex-col p-8'
+    :class='[price > 0 ? "bg-opacity-30 bg-primary" : ""]'
+  >
+    <NumberInput class='mb-2' :value='percentage' @input='handlePercentageChange' :max=100 />
+    <ItemPrice :price='price' :class='{"text-gray-600": price == 0}' />
+  </div>
+</template>
+<script setup lang='ts'>
+import ItemPrice from '@/components/ItemPrice.vue'
+import NumberInput from '@/components/inputs/NumberInput.vue'
+
+defineProps({
+  price: { type: Number, default: 0 },
+  percentage: { type: Number, default: 0 },
+})
+
+const emit = defineEmits(['update:percentage'])
+
+const handlePercentageChange = (newValue: number) => {
+  emit('update:percentage', newValue)
+}
+</script>
