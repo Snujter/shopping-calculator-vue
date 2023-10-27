d8b01eede410758fe20b0fb3f6121e983b938f95
Tamas Schneider <schneider.tamas1995@gmail.com>
2023-10-27 01:34:08 +0100

    Add ToggleInput component

---
+++ src/components/inputs/ToggleInput.spec.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect, assert } from 'vitest'
+
+import { shallowMount } from '@vue/test-utils'
+import ToggleInput from '@/components/inputs/ToggleInput.vue'
+
+describe('ToggleInput.vue', () => {
+  it('has default prop values', () => {
+    const wrapper = shallowMount(ToggleInput)
+
+    assert.equal(wrapper.props().isChecked, false, 'Default isChecked is not false')
+  })
+
+  it('renders button as disabled when prop is true', () => {
+    const wrapper = shallowMount(ToggleInput, {
+      props: {
+        isChecked: true
+      }
+    })
+
+    const $checkbox = wrapper.find('[data-test="checkbox"]').element as HTMLInputElement
+    expect($checkbox.checked).toBe(true)
+  })
+
+  it('renders button as not disabled when prop is false', () => {
+    const wrapper = shallowMount(ToggleInput, {
+      props: {
+        isChecked: false
+      }
+    })
+    const $checkbox = wrapper.find('[data-test="checkbox"]').element as HTMLInputElement
+    expect($checkbox.checked).toBe(false)
+  })
+
+  it('emits events', async () => {
+    const wrapper = shallowMount(ToggleInput)
+
+    // events to emit
+    wrapper.vm.$emit('update:is-checked', true)
+
+    // wait until emitted
+    await wrapper.vm.$nextTick()
+
+    // check if emitted values are correct
+    expect(wrapper.emitted('update:is-checked')?.[0]).toStrictEqual([true])
+  })
+})
