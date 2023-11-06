7a8db7a6c1d3348db79730632b3ee665cb34703a
Snajder <snaaajder@gmail.com>
2023-11-06 15:40:30 +0000

    Update TableHeader to be more reusable

--- src/components/TableHeader.vue
+++ src/components/TableHeader.vue
@@ -1,9 +1,35 @@
 <template>
-  <th class='
-    sticky z-10 top-0
-    px-4 py-4 tracking-widest font-normal
-    text-text bg-primary border-4 border-background
-  '>
-    <slot/>
-  </th>
+  <component
+    :is='containerElement'
+    class='
+      sticky z-10
+      px-4 py-4 tracking-widest font-normal
+      text-text
+    '
+    :class='[
+      [noBackground ? "bg-background" : "bg-gradient-to-br from-primary to-outline"],
+      {
+        "top-0": stickTo === ALIGNMENT.Top,
+        "bottom-0": stickTo === ALIGNMENT.Bottom
+      },
+    ]'
+  >
+    <slot />
+  </component>
 </template>
+
+<script setup lang='ts'>
+import { ALIGNMENT } from '@/globals'
+
+defineProps({
+  noBackground: { type: Boolean, default: false },
+  containerElement: { type: String, default: 'th' },
+  stickTo: {
+    type: Number as () => ALIGNMENT,
+    validator: (value: any): boolean => {
+      return value === ALIGNMENT.Top || value === ALIGNMENT.Bottom
+    },
+    default: ALIGNMENT.Top
+  }
+})
+</script>
--- src/globals.ts
+++ src/globals.ts
@@ -6,3 +6,10 @@ export enum PAYMENT_TYPES {
   Quantity,
   Percentage
 }
+
+export enum ALIGNMENT {
+  Top = 1,
+  Right = 2,
+  Bottom = 3,
+  Left = 4,
+}
