9243217154716aff554a193025346957d4e0bd52
Tamas Schneider <schneider.tamas1995@gmail.com>
2023-11-07 18:54:48 +0000

    Add TableFooter component

---
+++ src/components/TableFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <TableFrame
+    :stick-to='ALIGNMENT.Bottom'
+    container-element='td'
+    :no-background='noBackground'
+  >
+    <slot />
+  </TableFrame>
+</template>
+
+<script setup lang='ts'>
+import { ALIGNMENT } from '@/globals'
+import TableFrame from '@/components/TableFrame.vue'
+
+defineProps({
+  noBackground: { type: Boolean, default: false }
+})
+</script>
