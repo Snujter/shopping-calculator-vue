63600a5eb22531114013b44cebca8f8bcc002ad1
Snajder <snaaajder@gmail.com>
2023-11-11 03:26:20 +0000

    Add sidebar component

---
+++ src/components/AppSidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <aside
+    class='
+      absolute z-30 flex flex-col
+      p-2 w-14 hover:w-64 h-full
+      transition-all duration-300 ease-in-out
+      bg-background border-r-2 border-outline text-white
+    '
+  >
+    <div v-for='(group, index) in menuGroups' :key='index' class='my-2'>
+      <hr v-if='index !== 0' class='mb-4 border-gray-700' />
+      <ul>
+        <li
+          v-for='item in group'
+          :key='item.id'
+          @click='item.onClick'
+          class='left-0 flex items-center p-2 hover:bg-accent cursor-pointer overflow-hidden'
+        >
+          <component :is='item.icon' class='min-w-fit fill-text' />
+          <span class='text ml-2 truncate'>{{ item.text }}</span>
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<script setup lang='ts'>
+import { PropType, DefineComponent } from 'vue'
+
+defineProps({
+  menuGroups: {
+    type: Array as PropType<Array<Array<{
+      id: number;
+      icon: DefineComponent<{}, {}, any>;
+      text: string;
+      onClick: () => void
+    }>>>,
+    required: true
+  }
+})
+</script>
